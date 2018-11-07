d2e1e22846fe9ea5a07c50882ac9f13b1afd265e
Jan Löwenstrom <jloewenstrom@studenten.hs-bremerhaven.de>
2018-11-07 14:22:55 +0100

    separate print method for tests

---
+++ tests/short16.h
@@ -0,0 +1,6 @@
+#include <inttypes.h>
+struct short16{
+  uint8_t hi;
+  uint8_t lo;
+};
+
--- tests/validDate.c
+++ tests/validDate.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "validDate.h"
 #include <inttypes.h>
-uint8_t isValidDate(uint8_t day, uint8_t month, uint8_t year){
-  
-  return 0;
+
+uint8_t isValidDate(uint8_t day, uint8_t month, uint8_t yearHigh, uint8_t yearLow){
+  return 1;
 }
 
 
