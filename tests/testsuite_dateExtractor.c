ccddce255d8c08c5be4c343b16b2185d1c01c28f
superjoe74 <johann.h.h@web.com>
2018-12-19 13:32:38 +0100

    try fix extractor test

--- tests/testsuite_dateExtractor.c
+++ tests/testsuite_dateExtractor.c
@@ -43,6 +43,9 @@ void set3Bits(uint8_t i0, uint8_t i1, uint8_t b0, uint8_t b1, uint8_t b2) {
 void testGetMinute(){
     char* descr = "TEST- getMinute(): convert  bits 21-27 to responding decimal values";
 	DCF* dcf;
+	for(uint8_t i=0; i < 59; ++i){
+        dcf[i] = 0;
+    }
     set7Bits(dcf, 21, 28, 1,0,0,0,0,0,0);
     printTestResult(descr, getMinutes(&rawDCF) == 1, "0000001 should be 1");
 
